a3cf7f2744e851865e03f57957a18628bdf87003
José Luiz Freire de Macêdo <luizj.201@gmail.com>
2022-02-05 05:42:09 -0300

    adição de identação no base.css e introdução de js para o menu

---
+++ index.js
@@ -0,0 +1,8 @@
+const botaomenu = document.querySelector('.cabecalho__menu')
+const menu = document.querySelector('.menu__lateral')
+
+botaomenu.addEventListener('click', () => {
+
+  menu.classList.toggle('active__menu')
+
+})
